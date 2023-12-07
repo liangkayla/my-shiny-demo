d685cf7c66ae60f78be43ac82881f65392825a79
Kayla Liang <kayla.liang@duke.edu>
2023-12-07 13:20:26 -0500

    Finished class 12/07.

--- app.R
+++ app.R
@@ -15,15 +15,44 @@ shinyApp(
     sidebarLayout(
       sidebarPanel(
         # UI input code goes here
+        selectInput(
+          inputId = "city",
+          label = "Select city",
+          choices = c("Chicago", "Durham", "Sedona", "New York", "Los Angeles")
+        ),
+        selectInput(
+          inputId = "variable",
+          label = "Select variable",
+          choices = c("temp", "feelslike", "humidity")
+        )
       ),
       mainPanel(
         # UI output code goes here
+        tabsetPanel(
+          tabPanel(title = "Plot", plotOutput("plot")),
+          tabPanel(title = "Data", dataTableOutput("data"))
+        )
       )
     )
   ),
   server = function(input, output, session) {
 
-    # server code goes here
+    weather_city <- reactive({
+      weather |>
+        filter(city == input$city)
+    })
 
+    # server code goes here
+    output$plot <- renderPlot({weather_city() |>
+      ggplot(aes_string(x = "time", y = input$variable)) +
+      geom_line(color = "red") +
+      labs(title = paste(input$city, "-", input$variable))
+
+    })
+
+    output$data <- renderDataTable({
+      weather_city() |>
+        select(city, time, input$variable)
+    })
   }
 )
